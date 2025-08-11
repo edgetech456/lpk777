93c328645a4355a12db29adefdd803f5c6706401
weiyee <edgetechnology456@gmail.com>
2025-08-11 15:31:31 +0800

    add animation

--- inc/marquee.php
+++ inc/marquee.php
@@ -1,6 +1,6 @@
 <div class="marquee scrolling-container p-0 d-flex align-items-center mx-auto pnpm4-page-content-inner">
     <div class="mx-3">
-        <img src="images/bell.png" width="18px">
+        <img src="images/bell.png" width="18px" class="bell">
     </div>
     <div class="col" style="overflow:hidden;">
         <span class="h-100" width="90%"><strong>Deposit Setiap Bulan & Sertai Lucky Draw Bawa Pulang Sehingga MYR77,777</strong></span>
